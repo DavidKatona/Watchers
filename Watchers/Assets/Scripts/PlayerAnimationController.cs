7e14078ec38442d4344b12dba47bc619dd32a5f9
dave94punk <david.katona@hotmail.com>
2020-05-12 21:49:31 +0200

    Added project files to local repository.

---
+++ Watchers/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private Rigidbody2D playerRigidbody2d;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private PlayerStateList playerStateList;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetGroundedState();
+        SetFallingState();
+        SetRunningState();
+        SetJumpingState();
+        SetDashingState();
+    }
+
+    void SetGroundedState()
+    {
+        //Use the ground check method from the player movement script/component
+        if (playerMovement.GroundCheck())
+        {
+            playerAnimator.SetBool("IsGrounded", true);
+            playerAnimator.SetBool("IsFalling", false);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsGrounded", false);
+        }
+    }
+
+    void SetFallingState()
+    {
+        if (playerRigidbody2d.velocity.y < 0 && !playerMovement.GroundCheck())
+        {
+            playerAnimator.SetBool("IsFalling", true);
+        }
+    }
+
+    void SetRunningState()
+    {
+        //Running Animation
+        if (Mathf.Abs(playerRigidbody2d.velocity.x) > 0 && playerMovement.GroundCheck())
+        {
+            playerAnimator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsRunning", false);
+        }
+    }
+
+    void SetJumpingState()
+    {
+        //Jumping Animation
+        if (playerStateList.jumping && !playerStateList.wallSliding)
+        {
+            playerAnimator.SetBool("IsJumping", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsJumping", false);
+        }
+    }
+
+    void SetDashingState()
+    {
+        //Dash Animation
+        if (playerStateList.dashing)
+        {
+            playerAnimator.SetBool("IsDashing", true);
+        } else
+        {
+            playerAnimator.SetBool("IsDashing", false);
+        }
+    }
+}
---
+++ Watchers/Assets/Scripts/PlayerStateList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateList : MonoBehaviour
+{
+
+    public bool walking;
+    public bool interact;
+    public bool interacting;
+    public bool lookingRight;
+    public bool jumping;
+    public bool dashing;
+    public bool touchingWall;
+    public bool wallSliding;
+    public bool wallJumping;
+    public bool casting;
+    public bool castReleased;
+    public bool onBench;
+    public bool atBench;
+    public bool atNPC;
+    public bool usingNPC;
+    //public bool 
+
+}
