ec6acd61a3cbf35d2dc31665ae508245f6f7bcd7
dave94punk <david.katona@hotmail.com>
2020-07-17 12:09:45 +0200

    Added Attributes.

--- Watchers/Assets/Scripts/Attributes/Attribute.cs
+++ Watchers/Assets/Scripts/Attributes/Attribute.cs
@@ -1,30 +1,118 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Scripts.Attributes
 {
     [System.Serializable]
-    public class Attribute
+    public class Attributes
     {
-        [SerializeField] private int _value;
+        public event EventHandler OnAttributeChanged;
 
-        public int GetValue()
+        public static int ATTR_MIN = 0;
+        public static int ATTR_MAX = 99;
+
+        public enum AttributeType
         {
-            return _value;
+            Vigor,
+            Spirit,
+            Strength,
+            Intelligence,
+            Resilience,
+            Vitality,
+            Focus
         }
 
-        public void Decrement()
+        private SingleAttribute _vigor;
+        private SingleAttribute _spirit;
+        private SingleAttribute _strength;
+        private SingleAttribute _intelligence;
+        private SingleAttribute _resilience;
+        private SingleAttribute _vitality;
+        private SingleAttribute _focus;
+
+        public Attributes(int vigorAmount, int spiritAmount, int strengthAmount, int intelligenceAmount, int resilienceAmount, int vitalityAmount, int focusAmount)
         {
-            _value--;
+            _vigor = new SingleAttribute(vigorAmount);
+            _spirit = new SingleAttribute(spiritAmount);
+            _strength = new SingleAttribute(strengthAmount);
+            _intelligence = new SingleAttribute(intelligenceAmount);
+            _resilience = new SingleAttribute(resilienceAmount);
+            _vitality = new SingleAttribute(vitalityAmount);
+            _focus = new SingleAttribute(focusAmount);
         }
 
-        public void Increment()
+        private SingleAttribute GetSingleAttribute(AttributeType attributeType)
         {
-            _value++;
+            switch (attributeType)
+            {
+                default:
+                case AttributeType.Vigor:
+                    return _vigor;
+                case AttributeType.Spirit:
+                    return _spirit;
+                case AttributeType.Strength:
+                    return _strength;
+                case AttributeType.Intelligence:
+                    return _intelligence;
+                case AttributeType.Resilience:
+                    return _resilience;
+                case AttributeType.Vitality:
+                    return _vitality;
+                case AttributeType.Focus:
+                    return _focus;
+            }
         }
 
-        public Attribute(int baseValue)
+        public void SetAttribute(AttributeType attributeType, int attributeAmount)
         {
-            _value = baseValue;
+            GetSingleAttribute(attributeType).SetAttribute(attributeAmount);
+            OnAttributeChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void IncreaseAttribute(AttributeType attributeType)
+        {
+            SetAttribute(attributeType, GetAttributeAmount(attributeType) + 1);
+        }
+
+        public void DecreaseAttribute(AttributeType attributeType)
+        {
+            SetAttribute(attributeType, GetAttributeAmount(attributeType) - 1);
+        }
+
+        public int GetAttributeAmount(AttributeType attributeType)
+        {
+            return GetSingleAttribute(attributeType).GetAttributeAmount();
+        }
+
+        public float GetAttributeNormalized(AttributeType attributeType)
+        {
+            return GetSingleAttribute(attributeType).GetAttributeNormalized();
+        }
+
+        // Represents a single attribute of any type.
+        private class SingleAttribute
+        {
+            private int _attribute;
+
+            public SingleAttribute(int attributeAmount)
+            {
+                SetAttribute(attributeAmount);
+            }
+
+            public void SetAttribute(int attributeAmount)
+            {
+                _attribute = Mathf.Clamp(attributeAmount, ATTR_MIN, ATTR_MAX);
+            }
+
+            public int GetAttributeAmount()
+            {
+                return _attribute;
+            }
+
+            public float GetAttributeNormalized()
+            {
+                return (float)_attribute / ATTR_MAX;
+            }
         }
     }
 }
--- Watchers/Assets/Scripts/UI/CharacterMenu.cs
+++ Watchers/Assets/Scripts/UI/CharacterMenu.cs
@@ -1,14 +1,30 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Attributes;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterMenu : MonoBehaviour
 {
-    private bool _isOpened;
     public GameObject CharacterMenuUI;
     public AudioSource audioSource;
     public AudioClip menuOpenClip;
     public AudioClip menuCloseClip;
+    private bool _isOpened;
+    private Attributes _attributes;
 
-    // Update is called once per frame
+    public void SetAttributes(Attributes attributes)
+    {
+        _attributes = attributes;
+        _attributes.OnAttributeChanged += Attributes_OnAttributesChanged;
+        UpdateStatisticsVisuals();
+    }
+
+    private void Attributes_OnAttributesChanged(object sender, EventArgs e)
+    {
+        // Update visuals.
+        UpdateStatisticsVisuals();
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("OpenCharacterMenu"))
@@ -28,5 +44,16 @@ public class CharacterMenu : MonoBehaviour
 
             audioSource.Play();
         }
+    }
+
+    private void UpdateStatisticsVisuals()
+    {
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/VigorBackgroundColor/VigorCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Vigor).ToString();
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/SpiritBackgroundColor/SpiritCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Spirit).ToString();
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/StrengthBackgroundColor/StrengthCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Strength).ToString();
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/IntelligenceBackgroundColor/IntelligenceCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Intelligence).ToString();
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/ResilienceBackgroundColor/ResilienceCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Resilience).ToString();
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/VitalityBackgroundColor/VitalityCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Vitality).ToString();
+        transform.Find("CharacterMenuUI/CharacterMenuFrame/CharacterStatistics/Attributes/FocusBackgroundColor/FocusCounter").GetComponent<Text>().text = _attributes.GetAttributeAmount(Attributes.AttributeType.Focus).ToString();
     }
 }
