e6eaa53d90d0c8db4437c686e0c21e92afb3766b
dave94punk <david.katona@hotmail.com>
2020-07-22 21:10:30 +0200

    Changes to DamagePopup.

--- Watchers/Assets/Scripts/DamagePopups/DamagePopup.cs
+++ Watchers/Assets/Scripts/DamagePopups/DamagePopup.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class DamagePopup : MonoBehaviour
 {
+    private const float DISAPPER_TIMER_MAX = 0.5f;
+    private const float MOVE_VECTOR_MODIFIER = 2.5f;
     private TextMeshPro _textMesh;
     private float _disappearTimer;
     private Color _textColor;
+    private Vector3 _moveVector;
 
     public static DamagePopup Create(Vector3 position, int damageAmount)
     {
@@ -26,13 +29,28 @@ public class DamagePopup : MonoBehaviour
     {
         _textMesh.SetText(damageAmount.ToString());
         _textColor = _textMesh.color;
-        _disappearTimer = 0.5f;
+        _disappearTimer = DISAPPER_TIMER_MAX;
+
+        _moveVector = new Vector3(1, 1) * MOVE_VECTOR_MODIFIER;
     }
 
     private void Update()
     {
-        float yAxisSpeed = 5f;
-        transform.position += new Vector3(0, yAxisSpeed) * Time.deltaTime;
+        transform.position += _moveVector * Time.deltaTime;
+        _moveVector -= _moveVector * MOVE_VECTOR_MODIFIER * Time.deltaTime;
+
+        if (_disappearTimer > DISAPPER_TIMER_MAX * 0.5f)
+        {
+            // First half of the fade out animation.
+            float scaleModifier = 1f;
+            transform.localScale += Vector3.one * scaleModifier * Time.deltaTime;
+        }
+        else
+        {
+            // Second half of the fade out animation.
+            float scaleModifier = 1f;
+            transform.localScale -= Vector3.one * scaleModifier * Time.deltaTime;
+        }
 
         _disappearTimer -= Time.deltaTime;
 
