287c842b9391958f7ae846889b195b594226adbc
dave94punk <david.katona@hotmail.com>
2020-08-11 15:22:00 +0200

    Playing around with particles.

--- Watchers/Assets/Scripts/Damageables/Enemies/TestEnemy.cs
+++ Watchers/Assets/Scripts/Damageables/Enemies/TestEnemy.cs
@@ -36,16 +36,16 @@ namespace Assets.Scripts.Damagables.Enemies
             float unit = Health / localScale.x;
             localScale.x = (Health - damage) / unit;
             healthBar.localScale = localScale;
-            
+
             Health -= damage;
 
             StartCoroutine(Flash());
+            var hitEffect = Instantiate(GameAssets.GameAssets.i.prefabDeathEffect, transform.position, Quaternion.identity);
             _audioSource.clip = audioClipHit;
             _audioSource.Play();
 
             if (Health <= 0)
             {
-                Instantiate(GameAssets.GameAssets.i.prefabDeathEffect, transform.position, Quaternion.identity);
                 HitStop.Instance.Stop(0.04f);
                 Destroy(gameObject);
                 Died?.Invoke();
