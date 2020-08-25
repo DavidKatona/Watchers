00dea6af00dc7fffe459b57525114303c683ab31
dave94punk <david.katona@hotmail.com>
2020-08-25 12:47:33 +0200

    Slight changes to UI elements.

---
+++ Watchers/Assets/Scripts/Audio/OnHoverSoundPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class OnHoverSoundPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _audioClip;
+
+        public void PlayClip()
+        {
+            if (_audioSource != null)
+            {
+                _audioSource.clip = _audioClip;
+                _audioSource.Play();
+            }
+        }
+    }
+}
