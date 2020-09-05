f046ab49db7d9b82e2f981e5293cc936c0ac2a98
dave94punk <david.katona@hotmail.com>
2020-09-05 13:09:06 +0200

    Slight changes.

---
+++ Watchers/Assets/Scripts/UI/QuitGameButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class QuitGameButton : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(QuitGame);
+        }
+
+        private void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
